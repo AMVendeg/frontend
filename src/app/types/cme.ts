7e023b26452ad4bcfbd768669fc30ead3433d1c1
jones <mobilizacsio@gmail.com>
2022-07-30 20:08:21 +0200

    create types

---
+++ src/app/types/cme.ts
@@ -0,0 +1,12 @@
+export interface CME {
+  time21_5: string;
+  latitude: number;
+  halfAngle: number;
+  speed: number;
+  type: string;
+  isAccurateMost: boolean;
+  associatedCMEID: string;
+  note: string;
+  catalog: string;
+  link: string;
+}
