fdd18506aafe659a33724aefabda680a3f400b00
jones <mobilizacsio@gmail.com>
2022-07-30 20:16:52 +0200

    service getCmeData

--- src/app/services/sun-cme.service.ts
+++ src/app/services/sun-cme.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-// types
+import { CME } from '../types/cme';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +15,15 @@ export class SunCMEService {
   catalog = environment.catalog;
 
   constructor(private http: HttpClient) { }
+
+  getCmeData(
+    startDate: string, 
+    endDate: string, 
+    speed: number, 
+    halfAngle: number): Observable<CME[]> {
+    return this.http.get<CME[]>(
+      this.baseURL + `startDate=` + startDate + `&endDate=`+ endDate + 
+      `&speed=` + speed + `&halfAngle=`+ halfAngle + this.apiKey);
+  }
+
 }
